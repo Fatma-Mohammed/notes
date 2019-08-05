ac08519f836ef2cd83988f4c73617f3f01624879
fatma mohammed <gfatmamohammed@gmail.com>
2019-08-05 23:36:40 +0200

    add server page and connect to it

--- db.php
+++ db.php
@@ -4,10 +4,10 @@ $username = 'root';
 $password ='';
 $db_name = 'notes';
 
-$con = mysqli_connect($localhost,$username,$password,$db_name);
-if($con){
-    echo "done";
-}
-else{
-    echo "error";
-}
+// $con = mysqli_connect($localhost,$username,$password,$db_name);
+// if($con){
+//     echo "done";
+// }
+// else{
+//     echo "error";
+// }
---
+++ server.php
@@ -0,0 +1,6 @@
+<div class="card-panel note">
+    <i class="small material-icons right action">delete</i>
+    <i class="small material-icons right action">edit</i>
+    <h5>Fatma Mohammed</h5>
+    <small><i class="tiny material-icons">query_builder</i>5:00pm</small>
+</div>
