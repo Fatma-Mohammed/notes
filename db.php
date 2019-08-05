cd54f24e23c204fd98afd078b89a8719ddbfe47a
fatma mohammed <gfatmamohammed@gmail.com>
2019-08-05 20:20:33 +0200

    connect to database

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+$localhost = 'localhost';
+$username = 'root';
+$password ='';
+$db_name = 'notes';
+
+$con = mysqli_connect($localhost,$username,$password,$db_name);
+if($con){
+    echo "done";
+}
+else{
+    echo "error";
+}
