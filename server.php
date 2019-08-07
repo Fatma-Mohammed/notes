cb86ed0744a0afeead3aecb476bc1545e4d9ff08
fatma mohammed <gfatmamohammed@gmail.com>
2019-08-07 20:38:12 +0200

    making delet button

--- server.php
+++ server.php
@@ -3,6 +3,9 @@ include_once("db.php");
 $text=$_REQUEST['t'];
 $user=$_REQUEST['u'];
 $request=$_REQUEST['req'];
+$Id=$_REQUEST['d'];
+
+
 
 
 
@@ -12,10 +15,18 @@ if($request != ""){
         $ins="INSERT INTO note (user, noteText) VALUES ('$user', '$text')";
         mysqli_query($con,$ins);
     }
+    elseif($request == "del")
+    {
+        $de="DELETE FROM note WHERE id =$Id" ;
+        mysqli_query($con,$de);
+        
+    }
 }
-    $sel = "SELECT * FROM note ORDER BY id DESC";
-    $run = mysqli_query($con,$sel);
-    while($row=mysqli_fetch_assoc($run)){
+
+
+$sel = "SELECT * FROM note ORDER BY id DESC";
+$run = mysqli_query($con,$sel);
+while($row=mysqli_fetch_assoc($run)){
 
    
 
@@ -23,7 +34,7 @@ if($request != ""){
 
 
 <div class="card-panel note">
-    <i class="small material-icons right action">delete</i>
+    <i class="small material-icons right action" onclick= "post('del',<?php echo $row['id']?>)">delete</i>
     <i class="small material-icons right action">edit</i>
     <h5><?php echo $row["user"] ?></h5>
     <small><i class="tiny material-icons">query_builder</i><?php echo $row["time"]?></small><br>
